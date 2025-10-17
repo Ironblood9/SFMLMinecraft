327aec1dc65294ed074b952fc292aef0756b4446
Demirkan Çevik <demirkancevik48@gmail.com>
2025-10-17 23:33:10 +0300

    World.h and World.cpp

---
+++ SFMLMinecraft/World.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+#include "TileMap.h"
+#include "TileID.h"
+
+bool checkCollision(const sf::FloatRect& rect, const TileMap& map, const std::vector<int>& solidTiles);
+bool rectIntersects(const sf::FloatRect& rect1, const sf::FloatRect& rect2);
+int  randomInt(int min, int max);
+void generateWaterPool(std::vector<int>& tiles, unsigned int width, unsigned int height, int centerX, int centerY, int size);
+void generateLavaPool(std::vector<int>& tiles, unsigned int width, unsigned int height, int centerX, int centerY, int size);
+void generateUndergroundCave(std::vector<int>& tiles, unsigned int width, unsigned int height, int centerX, int centerY, int size);
+void generateCleanTerrainWithLiquids(std::vector<int>& tiles, unsigned int width, unsigned int height);
+void generateNiceTrees(std::vector<int>& tiles, unsigned int width, unsigned int height);
+void generateSimpleDetails(std::vector<int>& tiles, unsigned int width, unsigned int height);
+
